6b75fd6adbbbcac76c1e2be4be951f4a45d1a618
ivnmrn <ivanmarinmatos@hotmail.com>
2020-06-29 11:47:01 +0200

    selector button

--- image/testss.py
+++ image/testss.py
@@ -1,6 +1,12 @@
 from PIL import Image, ExifTags
+from PIL.ExifTags import TAGS, GPSTAGS
 
-def handle_uploaded_file(filepath):
+def cordenates(filepath):
     img = Image.open(filepath)
     exif = { ExifTags.TAGS[k]: v for k, v in img._getexif().items() if k in ExifTags.TAGS }
     print(exif)
+    # for (idx, tag) in TAGS.items():
+    #     if tag == 'GPSInfo':
+    #         for (key, val) in GPSTAGS.items():
+    #             if key in exif[idx]:
+    #                 print(val)
--- image/views.py
+++ image/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
 from  .forms import Image
-from .testss import handle_uploaded_file
+from .testss import cordenates
 
 def upload_file(request):
     if request.method == 'POST':
         form = Image(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(request.FILES['file'])
-            return HttpResponseRedirect('/polls/')
+            cordenates(request.FILES['file'])
+            return HttpResponseRedirect('test/')
     else:
         form = Image()
     return render(request, 'upload.html', {'form': form})

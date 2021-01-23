b041ef333dc9dded96199601ca8c10855a9f628a
ivan <ivanmarinmatos@hotmail.com>
2021-01-23 16:17:22 +0100

    delete blank spaces

--- geo_location/exif.py
+++ geo_location/exif.py
@@ -6,19 +6,16 @@ def get_decimal_from_dms(dms, ref):
     degrees = dms[0][0] / dms[0][1]
     minutes = dms[1][0] / dms[1][1] / 60.0
     seconds = dms[2][0] / dms[2][1] / 3600.0
-
     if ref in ['S', 'W']:
         degrees *= -1
         minutes *= -1
         seconds *= -1
-
     return round(degrees + minutes + seconds, 5)
 
 
 def get_latitude_longitude(geo_tags):
     lat = get_decimal_from_dms(geo_tags['GPSLatitude'], geo_tags['GPSLatitudeRef'])
     lon = get_decimal_from_dms(geo_tags['GPSLongitude'], geo_tags['GPSLongitudeRef'])
-
     return dict(latitude=lat, longitude=lon)
 
 
@@ -27,14 +24,12 @@ def get_degrees(exif):
     for key, val in exif['GPSInfo'].items():
         if GPSTAGS[key][:4] == 'GPSL':
             geo[GPSTAGS[key]] = val
-
     return geo
 
 
 def handle_uploaded_image(image):
     img = Image.open(image)
     exif = {}
-
     if img._getexif():
         for key, value in img._getexif().items():
             if key in ExifTags.TAGS and key != 37500:
